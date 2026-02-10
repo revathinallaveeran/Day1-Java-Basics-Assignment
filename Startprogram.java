db0b4af3ff33d057bef94ec9c995b4239431f67d
revathinallaveeran <revathinallaveeran@gmail.com>
2026-02-10 10:22:34 +0530

    Add files via upload

---
+++ Startprogram.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class Startprogram {
+
+	public static void main(String[] args) {
+		
+        System.out.println("    * * *");
+
+        
+        System.out.println("  * * * * *");
+
+	}
+
+}
