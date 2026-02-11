2437e9b01d980d919b252cef84d466aefb227d2b
revathinallaveeran <revathinallaveeran@gmail.com>
2026-02-11 18:09:18 +0530

    Add files via upload

--- Day3/Decendingorder.java
+++ Day3/Decendingorder.java
@@ -5,16 +5,9 @@ import java.util.Scanner;
 public class Decendingorder {
 
 	public static void main(String[] args) {
-	
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the number");
-		int num = scan.nextInt();
-		
-		for(int i = num; i >= 1; i--) {
+		for(int i = 10; i >= 1; i--) {
 			System.out.println(i);
 		}
-		
-		scan.close();
 
 	}
 
